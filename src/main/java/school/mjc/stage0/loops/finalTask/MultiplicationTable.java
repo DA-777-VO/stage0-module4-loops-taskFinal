01f829d6c3eaccbe98604ea1f3d96e2049192f36
David Apakin <apakin.david@gmail.com>
2023-11-09 19:25:23 +0400

    4.2.final

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+
+
+        for (int j = 1; j < 10; j++) {
+            System.out.println(numberTableToPrint + "*" + j + "=" + (numberTableToPrint * j));
+        }
+
+
     }
 }
